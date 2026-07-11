bdedbc58a1313aaa8faf36d97719a472a311712c
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-07-11 10:38:24 +0530

    Added first bubble sort swammping problem

---
+++ src/Bubble_sort/Sorting.java
@@ -0,0 +1,24 @@
+package Bubble_sort;
+import java.util.Arrays;
+public class Sorting {
+    public static void main(String[] args) {
+       int [] nums = {3,4,5,1,2};
+      int [] ans = Sort(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] Sort(int [] nums){
+        int n = nums.length -1;
+        //Time complexity O(n2).
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < nums.length -i -1; j++){
+                if(nums[j + 1] < nums [j]){
+                    int temp = nums[j];
+                    //now swapping the adjecent sides.
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
