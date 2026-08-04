c54ff226cded1edd55402cdae0398082a8053841
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-04 10:56:02 +0530

    Added String comparisions with reference variables and object values

--- src/Strings/Comparison.java
+++ src/Strings/Comparison.java
@@ -1,10 +1,13 @@
 package Strings;
 
 public class Comparison {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         String a = "sunil";
         String b = "sunil";
         check(a,b);
+        String name1 = new String("sunil");
+        String name2 = new String("sunil");
+        compare(name1,name2);
     }
     static void check(String a,String b){
         if(a == b){
@@ -14,4 +17,15 @@ public class Comparison {
             System.out.println(false);
         }
     }
+    static void compare(String name1,String name2){
+        //if(name1.equals == name2.equals);
+        //Here we are checking the object values are equals,using equals method which java provided"
+        if(name1.equals(name2)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("Nope!");
+        }
+    }
+
 }
