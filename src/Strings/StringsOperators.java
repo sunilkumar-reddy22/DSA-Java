34c13ae0753a2145d066e7585d253a3643a46bdd
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-07 20:19:38 +0530

    Added Strings operaror

---
+++ src/Strings/StringsOperators.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class StringsOperators {
+    public static void main(String[] args) {
+        String name = "sunil";
+        String name1 = " kumar reddy";
+        System.out.println(name + name1);
+    }
+}
