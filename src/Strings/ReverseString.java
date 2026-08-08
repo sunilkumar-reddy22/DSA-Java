59efc7d2541a9f62d61031adb45487a632b6eca3
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-08 17:55:58 +0530

    Added reverse a String

--- src/Strings/ReverseString.java
+++ src/Strings/ReverseString.java
@@ -1,7 +1,22 @@
 package Strings;
-
+import java.util.Arrays;
 public class ReverseString {
     public static void main(String[] args) {
-
+       //char array
+        char [] s = {'h', 'e', 'l', 'l', 'o'};
+       char []  ans = Reverse(s);
+        System.out.println(Arrays.toString(ans));
+    }
+    static char [] Reverse(char [] s){
+        int left = 0;
+        int right = s.length -1;
+        while(left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        return s;
     }
 }
