e031eea503426c319cb88a5390500fddfa5c7935
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-11 21:55:58 +0530

    checking Indexof method calling

---
+++ src/Strings/CallingIndexOf.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class CallingIndexOf {
+    public static void main(String[] args) {
+        String name = "sunil kumar reddy";
+        index(name);
+    }
+    static void index(String name){
+        StringBuilder sb = new StringBuilder(name);
+      int ans =  sb.lastIndexOf("u");
+        System.out.println(ans);
+        System.out.println(sb.toString());
+    }
+}
