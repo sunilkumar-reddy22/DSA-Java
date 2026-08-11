cb1fe7caf9784e3fc503d65d9cdd15bee3faf6c1
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-11 21:34:23 +0530

    Added reverse stringsII Leetcode problem

--- src/Strings/ReverseStringsTwo.java
+++ src/Strings/ReverseStringsTwo.java
@@ -5,21 +5,24 @@ import static Strings.ReverseWords.reverse;
 public class ReverseStringsTwo {
     public static void main(String[] args) {
         String s = "Let's take LeetCode contest";
-       String ans = Reverse(s);
+        String ans = Reverse(s);
         System.out.println(ans);
     }
-    static String Reverse(String s){
-        String [] words = s.trim().split("\\s+");
+
+    static String Reverse(String s) {
+        String[] words = s.trim().split("\\s+");
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < words.length; i++){
-           sb.append(reverse(words[i]));
-            if( i < words.length -1){
+        for (int i = 0; i < words.length; i++) {
+            sb.append(reverse(words[i]));
+            if (i < words.length - 1) {
                 sb.append(" ");
             }
         }
         return sb.toString();
     }
+
     static String reverse(String word) {
         return new StringBuilder(word).reverse().toString();
     }
 }
+
