eef46cc13595107a5b855b9d1112660c7613e5e4
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-20 20:44:34 +0530

    Added caret pattren

--- src/Rvise_pattrens/RectanglePattren2.java
+++ src/Rvise_pattrens/RectanglePattren2.java
@@ -4,13 +4,30 @@ public class RectanglePattren2 {
     public static void main(String[] args) {
         int n = 5;
         angle(n);
+        Triangle(n);
     }
-    static void angle(int n){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <= n; j++){
+
+    static void angle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
+           // System.out.println();
+
         }
+        System.out.println("\n");
+    }
+
+    static void Triangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("^ ");
+            }
+            System.out.println();
+        }
+
     }
 }
+
+
