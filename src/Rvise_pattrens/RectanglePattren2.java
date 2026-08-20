adf45d2a76af8e4e180227a0bb6944e0b180ba14
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-20 18:44:40 +0530

    added rectangle pattren

---
+++ src/Rvise_pattrens/RectanglePattren2.java
@@ -0,0 +1,16 @@
+package Rvise_pattrens;
+
+public class RectanglePattren2 {
+    public static void main(String[] args) {
+        int n = 5;
+        angle(n);
+    }
+    static void angle(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
