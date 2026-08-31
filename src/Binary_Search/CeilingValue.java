ca42c4895b44c5d759d84d4ed3beea5ba0aee9d9
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-08-31 11:14:10 +0530

    added edge case to floorvalue program

--- src/Binary_Search/CeilingValue.java
+++ src/Binary_Search/CeilingValue.java
@@ -1,32 +1,33 @@
 package Binary_Search;
 
-public class FloorValue {
+public class CeilingValue {
     public static void main(String[] args) {
-        int [] nums = {1,3,4,5,7};
-        int target = 9; //in floor ans need to give 5.
-       int ans = Floor(nums,target);
+        int [] nums = {1,2,3,4,5,6,7,9};
+        int target = 8;
+       int ans = Ceiling(nums,target);
         System.out.println(ans);
     }
-    static int Floor(int [] nums,int target){
+    static int Ceiling(int [] nums , int target){
         int ans = 0;
         int start = 0;
         int end = nums.length -1;
         while(start <= end){
             int mid = start +(end - start)/2;
-            if(target <= 0){
+            if(target > nums[nums.length -1]){
                 return -1;
             }
             if(nums[mid] == target){
-                return target;
+                return mid;
             }
-            else if(nums[mid] < target){
+            else if(nums[mid] > target){
                 ans = mid;
-                start = mid + 1;
+                end = mid -1;
             }
             else{
-                end = mid -1;
+                start = mid + 1;
             }
         }
         return ans;
+
     }
 }
