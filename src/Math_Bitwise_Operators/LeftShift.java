37df4058453d1e1d13a7e94dc8fb624e59a9d5de
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-09-03 21:24:01 +0530

    did basic left and right shif operator

---
+++ src/Math_Bitwise_Operators/LeftShift.java
@@ -0,0 +1,12 @@
+package Math_Bitwise_Operators;
+
+public class LeftShift {
+    public static void main(String[] args) {
+        int a = 4;
+        add(a);
+    }
+    static void add(int a){
+        System.out.println(4 << 2);
+        System.out.println( 8 >> 2);
+    }
+}
